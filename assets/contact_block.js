12255c52ba5db9e09e702841f053535ef117f29c
itmaroon <master@itmaroon.net>
2023-08-01 08:29:31 +0900

    8-1

--- assets/contact_block.js
+++ assets/contact_block.js
@@ -46,7 +46,7 @@ jQuery(function ($) {
     return message
   }
   //formの送信ボタンが押されたときの処理
-  $('#guest_contact_form').on('submit', function (e) {
+  $('#to_confirm_form').on('submit', function (e) {
     e.preventDefault();
     let err_flg = false;//エラーフラグをセット
     //バリデーションチェック
@@ -62,34 +62,99 @@ jQuery(function ($) {
         }
       }
     })
-    let use_name = $('input[name="user_name"]').val()
-    console.log(use_name);
     if (err_flg) return;//エラーフラグがtrueなら処理終了
+    //次の画面に遷移
+    $(this).parent().removeClass('appear');
+    $(this).parent().next().addClass('appear');
+    //プログレスエリアの処理
+    let classNames = $(this).parent().next().attr('class').split(/\s+/);  // クラス名を空白文字で分割
+    let wpBlockClasses = $.grep(classNames, function (className) {  // 'wp-block'で始まるクラス名を抽出
+      return /^wp-block/.test(className);
+    });
 
+    let lis = $('.wp-block-itmar-design-process').find('li');
+    lis.each(function () {
+      var liClasses = $(this).attr('class').split(/\s+/);
+      console.log('liClasses:', liClasses);
+      console.log('wpBlockClasses[0]:', wpBlockClasses[0]);
+      var some_result = liClasses.some(function (liClass) {
+        var result = wpBlockClasses[0].includes(liClass);
+        return result;
+      });
+      console.log('some_result:', some_result);
+    });
+    // var targetLis = lis.filter(function () {
+    //   var liClasses = $(this).attr('class').split(/\s+/);  // li要素のクラス名を取得し配列にする
+    //   return liClasses.some(function (liClass) {
+    //     var result = wpBlockClasses[0].includes(liClass);  // 'wp-block'で始まるクラス名がliのクラス名を含むかチェック
+    //     return result;
+    //   });
+    // });
+    // console.log(targetLis);
 
-    let master_email = $(this).data('master_mail');
-    let subject_info = $(this).data('subject_info');
-    let message_info = $(this).data('message_info');
-    let is_dataSave = $(this).data('is_datasave');
-    //message_infoの再構築
-    message_info = message_rebuild(message_info);
-    //通知メールの送信
-    sendMail_ajax(master_email, subject_info, message_info, master_email, false, false);
-    //自動応答メール
-    let is_retmail = $(this).data('is_retmail');
-    if (is_retmail) {
-      let ret_inputName = $(this).data('ret_mail');
-      let ret_email = $(`[name="${ret_inputName}"]`).val()
-      let master_email = $(this).data('master_mail');
-      let subject_ret = $(this).data('subject_ret');
-      let message_ret = $(this).data('message_ret');
-      //message_retの再構築
-      message_ret = message_rebuild(message_ret);
-      //自動応答メールの送信
-      sendMail_ajax(ret_email, subject_ret, message_ret, master_email, is_dataSave, true);
-    }
+    //確認データの表示
+    let input_elm = $(this).find('input:not([type="submit"]), textarea')
+    input_elm.each(function (index) {
+      let input_val = $(this).val();
+      $('#send_exec').find('tr').eq(index).find('td').text(input_val);
+    });
+  });
 
+  //送信ボタンの実行かキャンセルか
+  let send_click_btn;
+  $("#send_exec_btn").click(function () {
+    send_click_btn = "send_exec_btn";
+  });
+
+  $("#send_cancel_btn").click(function () {
+    send_click_btn = "send_cancel_btn";
   });
 
 
+  $('#send_exec').on('submit', function (e) {
+    e.preventDefault();
+    //キャンセルボタンなら元に戻して終了
+    if (send_click_btn === 'send_cancel_btn') {
+      $(this).parent().removeClass('appear');
+      $(this).parent().prev().addClass('appear');
+      return;
+    }
+
+    //親ブロックの情報取得
+    let parent_block = $(this).parents('.wp-block-itmar-guest-contact-block');
+
+    let master_email = parent_block.data('master_mail');
+    let subject_info = parent_block.data('subject_info');
+    let message_info = parent_block.data('message_info');
+    let is_dataSave = parent_block.data('is_datasave');
+    //message_infoの再構築
+    message_info = message_rebuild(message_info);
+    //通知メールの送信
+    //sendMail_ajax(master_email, subject_info, message_info, master_email, false, false);
+    //自動応答メール
+    let is_retmail = parent_block.data('is_retmail');
+    if (is_retmail) {
+      let ret_inputName = parent_block.data('ret_mail');
+      let ret_email = $(`[name="${ret_inputName}"]`).val()
+      let master_email = parent_block.data('master_mail');
+      let subject_ret = parent_block.data('subject_ret');
+      let message_ret = parent_block.data('message_ret');
+      //message_retの再構築
+      message_ret = message_rebuild(message_ret);
+      //自動応答メールの送信
+      //sendMail_ajax(ret_email, subject_ret, message_ret, master_email, is_dataSave, true);
+    }
+
+    //画面遷移
+    $(this).parent().removeClass('appear');
+    $(this).parent().next().addClass('appear');
+  });
+
+  //ホームに戻るボタンの処理
+  $('#to_home').on('submit', function (e) {
+    e.preventDefault();
+    //画面遷移
+    $(this).parent().removeClass('appear');
+    $(this).parent().siblings('.first_appear').addClass('appear');
+  });
 });
